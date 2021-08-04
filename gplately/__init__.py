ad44be8c2f5aa536d0d367d04645ebb9ebf331fe
lauren-ilano <lila0720@uni.sydney.edu.au>
2021-08-04 16:22:08 +1000

    import data.py & DataServer

--- gplately/__init__.py
+++ gplately/__init__.py
@@ -1,8 +1,10 @@
 from . import (
     grids,
     plot,
+    data,
 )
 from .grids import Raster, TimeRaster
 from .plot import PlotTopologies
 from .reconstruction import PlateReconstruction, Points
 from .tools import EARTH_RADIUS
+from .data import DataServer
