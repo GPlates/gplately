8264f7ed6a304f31aad72a352814186a50728bb3
Ben Mather <brmather1@gmail.com>
2022-02-03 11:51:09 +1100

    future-proof python versions

--- setup.py
+++ setup.py
@@ -101,6 +101,8 @@ if __name__ == "__main__":
                                'Programming Language :: Python :: 3.5',
                                'Programming Language :: Python :: 3.6',
                                'Programming Language :: Python :: 3.7',
-                               'Programming Language :: Python :: 3.8'
+                               'Programming Language :: Python :: 3.8',
+                               'Programming Language :: Python :: 3.9',
+                               'Programming Language :: Python :: 3.10',
                                ]
           )
