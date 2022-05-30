4aa945bb3446435eb8f73ebb085cbb96c62d4ffc
lauren-ilano <lila0720@uni.sydney.edu.au>
2022-05-30 14:51:22 +1000

    add mor seedpoint function

--- gplately/oceans.py
+++ gplately/oceans.py
@@ -10,7 +10,7 @@ import glob
 
 from . import reconstruction
 from . import plot
-
+from ptt import separate_ridge_transform_segments
 #
 #
 #
@@ -28,8 +28,11 @@ from . import plot
 #
 #
 #
-#
-#
+# How time is iterated through. will we call an object once per time? or will a list iterate through an attributed min, max time?
+# min_time: 0.
+#    max_time: 170.
+#    mor_time_step: 1.
+#    gridding_time_step: 1.
 #
 #
 #
@@ -44,14 +47,18 @@ class SeafloorGrid(object):
         PlateReconstruction_object=None, 
         PlotTopologies_object=None,
         refinement_levels=5, 
+        ridge_sampling=0.1,
+        ridge_time_step=1.,
         time=0):
 
         # Provides a rotation model, topology features and reconstruction time for 
         # the SeafloorGrid
         self.PlateReconstruction_object = PlateReconstruction_object
-        self.PlotTopologies_object = PlotTopologies_object
+        self._PlotTopologies_object = PlotTopologies_object
         self.refinement_levels = refinement_levels
-        self.time = float(time)
+        self.ridge_sampling = ridge_sampling
+        self._time = time
+        # self.PlotTopologies_object.time = self._time
 
         ## TO DO LATER:
         # The following lines will have to deal with how the input data looks like - 
@@ -78,6 +85,33 @@ class SeafloorGrid(object):
         self.lons = np.linspace(extent[0], extent[1], self.data.shape[1])
         self.lats = np.linspace(extent[2], extent[3], self.data.shape[0])
         """
+
+    # Allow SeafloorGrid time to be updated, and to update the internally-used 
+    # PlotTopologies' time attribute too. If PlotTopologies is used outside the
+    # object, its `time` attribute is not updated. 
+    @property
+    def time(self):
+        """ The reconstruction time."""
+        return self._time
+
+
+    @property
+    def PlotTopologiesTime(self):
+        return self._PlotTopologies_object.time
+
+
+    @time.setter
+    def time(self, var):
+        if var >= 0:
+            self.update_time(var)
+        else:
+            raise ValueError("Enter a valid time >= 0")
+
+
+    def update_time(self, time):
+        self._time = float(time)
+        self._PlotTopologies_object.time = float(time)
+
 
 
     def get_initial_ocean_seed_points(self, save_directory=None):
@@ -113,7 +147,7 @@ class SeafloorGrid(object):
         # Collect continental polygon features and reconstructed geometries
         polygons = []
         polygon_features = []
-        for reconstructed_continental_geometry in self.PlotTopologies_object.continents:
+        for reconstructed_continental_geometry in self._PlotTopologies_object.continents:
             polygons.append(
                 reconstructed_continental_geometry.get_reconstructed_geometry()
             )
@@ -160,3 +194,88 @@ class SeafloorGrid(object):
         elif masking == 'Inside':
         pg_points = pg_points[1]
         """
+
+
+    def get_mid_ocean_ridge_seedpoints(self):
+        """ Resolve mid-ocean ridges to the current `time`, and shift their shared sub-segments
+        them slightly off the ridge using their stage rotation. 
+
+        Adapted from an age gridding workflow by Simon Williams, John Cannon and Nicky Wright.
+
+        Returns
+        -----
+        shifted_mor_points : list of pygplates.PointOnSphere objects
+            A list containing ridge seed points that have been slightly rotated away from
+            ridge locations at the current timestep.
+        """
+
+        # Get the rotation model ascribed to the plate model. Topology features
+        # are already resolved to `time`. 
+        rotation_model = self.PlateReconstruction_object.rotation_model
+        topology_features = self.PlateReconstruction_object.topology_features
+        topology_features_extracted = pygplates.FeaturesFunctionArgument(topology_features)
+
+        # Resolve topologies to the current time.
+        resolved_topologies = []
+        shared_boundary_sections = []
+        pygplates.resolve_topologies(
+            topology_features_extracted.get_features(), 
+            rotation_model, 
+            resolved_topologies, 
+            self.time, 
+            shared_boundary_sections
+        )
+        shifted_mor_points = []
+
+        # pygplates.ResolvedTopologicalSection objects.
+        for shared_boundary_section in shared_boundary_sections:
+            if shared_boundary_section.get_feature().get_feature_type() == pygplates.FeatureType.create_gpml('MidOceanRidge'):
+                
+                spreading_feature = shared_boundary_section.get_feature()
+
+                # Find the stage rotation of the spreading feature in the 
+                # frame of reference of its geometry at the current 
+                # reconstruction time (the MOR is currently actively spreading).
+                # The stage pole can then be directly geometrically compared 
+                # to the *reconstructed* spreading geometry.
+                stage_rotation = separate_ridge_transform_segments.get_stage_rotation_for_reconstructed_geometry(
+                    spreading_feature, rotation_model, self.time
+                )
+                if not stage_rotation:
+                    # Skip current feature - it's not a spreading feature.
+                    continue
+
+                # Get the stage pole of the stage rotation.
+                # Note that the stage rotation is already in frame of 
+                # reference of the *reconstructed* geometry at the spreading time.
+                stage_pole, _ = stage_rotation.get_euler_pole_and_angle()
+
+                # One way rotates left and the other right, but don't know 
+                # which - doesn't matter in our example though.
+                rotate_slightly_off_mor_one_way = pygplates.FiniteRotation(
+                    stage_pole, 
+                    np.radians(0.01)
+                )
+                rotate_slightly_off_mor_opposite_way = rotate_slightly_off_mor_one_way.get_inverse()
+                
+                # Iterate over the shared sub-segments.
+                for shared_sub_segment in shared_boundary_section.get_shared_sub_segments():
+
+                    # Tessellate MOR section.
+                    mor_points = pygplates.MultiPointOnSphere(
+                        shared_sub_segment.get_resolved_geometry().to_tessellated(np.radians(self.ridge_sampling))
+                    )
+                    for point in mor_points.get_points()[1:-1]:
+                        # Append shifted geometries (one with points rotated one way 
+                        # and the other rotated the opposite way).
+                        shifted_mor_points.append(
+                            rotate_slightly_off_mor_one_way * point
+                        )
+                        shifted_mor_points.append(
+                            rotate_slightly_off_mor_opposite_way * point    
+                        )
+        return shifted_mor_points
+
+
+
+
