126c3971a16771cb846464f797e11c5911b242fd
lauren-ilano <lila0720@uni.sydney.edu.au>
2022-12-07 15:17:40 +1100

    add initial tests for SeafloorGrid

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 import gplately
 import numpy as np
+import os
+import tempfile
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from gplately import DataCollection
@@ -10,6 +12,7 @@ from gplately import DataCollection
 # We will test GPlately functionalities on the Müller et al. (2019) plate reconstruction
 # model at 0 and 100 Ma.
 reconstruction_times = [0, 100]
+gridding_times = [249., 250.]
 pt_lon = np.array([-155.4696, 164.3])
 pt_lat = np.array([19.8202, 53.5])
 
@@ -56,3 +59,41 @@ def gplately_raster_object(gplately_plate_reconstruction_object):
 
     graster = gplately.Raster(model, array=masked_age_grid, extent=[-180,180,-90,90])
     return graster
+
+
+# Create a temporary directory for testing seafloorgrid
+#@pytest.fixture(scope="module")
+#def temp_save_directory():
+#    tmpdir = tempfile.mkdtemp()
+#    os.mkdir(tmpdir, exist_ok=True)
+#    return tmpdir
+
+
+@pytest.fixture(scope="session")
+def test_save_directory(tmp_path_factory):
+    parent_dir = tmp_path_factory.mktemp("seafloorgrid_test")
+    return parent_dir
+
+
+@pytest.fixture(scope="module")
+def gplately_seafloorgrid_object(
+    gplately_plate_reconstruction_object, 
+    gplately_plot_topologies_object, 
+    test_save_directory
+):
+    
+    model = gplately_plate_reconstruction_object
+    gplot = gplately_plot_topologies_object
+
+    seafloorgrid = gplately.SeafloorGrid(
+        model, 
+        gplot, 
+        max_time = 250,
+        min_time = 249,
+        ridge_time_step=1.,
+        save_directory=str(test_save_directory),
+        file_collection = "Muller2019",
+        spacing_degrees = 0.25
+    )
+    return seafloorgrid
+
