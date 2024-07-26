c5ede1024a4d6a884f741b328edb25eae0f8f4a1
michaelchin <michaelchin78@hotmail.com>
2024-07-26 13:19:09 +1000

    update test_crustal_production.py

--- tests-dir/unittest/test_crustal_production.py
+++ tests-dir/unittest/test_crustal_production.py
@@ -6,12 +6,25 @@ from common import *
 import gplately
 
 print(gplately.__file__)
+from plate_model_manager.utils import download
+
 from gplately.utils import crustal_production
 
+data_dir = "./crustal_production_agegrid"
 if __name__ == "__main__":
-    # https://www.earthbyte.org/webdav/ftp/Data_Collections/Muller_etal_2019_Tectonics/Muller_etal_2019_Agegrids/Muller_etal_2019_Tectonics_v2.0_netCDF/Muller_etal_2019_Tectonics_v2.0_AgeGrid-0.nc
+    downloader = download.FileDownloader(
+        "https://www.earthbyte.org/webdav/ftp/Data_Collections/Muller_etal_2019_Tectonics/Muller_etal_2019_Agegrids/Muller_etal_2019_Tectonics_v2.0_netCDF/Muller_etal_2019_Tectonics_v2.0_AgeGrid-0.nc",
+        f"{data_dir}/.metadata.json",
+        f"{data_dir}",
+    )
+    # only re-download when necessary
+    if downloader.check_if_file_need_update():
+        downloader.download_file_and_update_metadata()
+    else:
+        print(f"The local age grid is still good. No need to download again!")
+
     for i in range(3, 10):
         crustal_production_rate = crustal_production.compute_crustal_production(
-            "Muller_etal_2019_Tectonics_v2.0_AgeGrid-0.nc", i
+            f"{data_dir}/Muller_etal_2019_Tectonics_v2.0_AgeGrid-0.nc", i
         )
         print(f"{i}: {crustal_production_rate}")
