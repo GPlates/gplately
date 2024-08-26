f277a069877cda7ee94e779415b41a8775b944a0
michaelchin <michaelchin78@hotmail.com>
2024-08-26 12:57:04 +1000

    check in some experimental code

---
+++ gplately/commands/reset_feature_type.py
@@ -0,0 +1,15 @@
+import pygplates
+
+new_fc = pygplates.FeatureCollection()
+for f in pygplates.FeatureCollection("shapes_continents.gpml"):
+    # print(f.get_feature_type())
+    new_f = pygplates.Feature(
+        pygplates.FeatureType.gpml_unclassified_feature,
+        f.get_feature_id(),
+        pygplates.VerifyInformationModel.yes,
+    )
+    for p in f:
+        new_f.add(p.get_name(), p.get_value())
+    new_fc.add(new_f)
+
+new_fc.write("new_shapes_continents.gpmlz")
