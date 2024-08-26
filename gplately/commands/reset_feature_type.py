e1ce4c1dbbec6a7c24caecd3bdb8422e985fcf34
michaelchin <michaelchin78@hotmail.com>
2024-08-26 17:27:48 +1000

    minor changes according to JC comments

--- gplately/commands/reset_feature_type.py
+++ gplately/commands/reset_feature_type.py
@@ -6,10 +6,14 @@ for f in pygplates.FeatureCollection("shapes_continents.gpml"):
     new_f = pygplates.Feature(
         pygplates.FeatureType.gpml_unclassified_feature,
         f.get_feature_id(),
-        pygplates.VerifyInformationModel.yes,
+        pygplates.VerifyInformationModel.yes,  # in case the user provided a wrong feature type
     )
     for p in f:
-        new_f.add(p.get_name(), p.get_value(), pygplates.VerifyInformationModel.yes)
+        new_f.add(
+            p.get_name(),
+            p.get_time_dependent_value(),
+            pygplates.VerifyInformationModel.no,
+        )
     new_fc.add(new_f)
 
 new_fc.write("new_shapes_continents.gpmlz")
