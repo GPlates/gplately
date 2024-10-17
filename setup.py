7c44500ef02c8a87f29f33e79acd053fea267c30
michaelchin <michael.chin@sydney.edu.au>
2024-10-17 14:47:53 +1100

    add python 3.8 as the minimal version requirement (#293)

--- setup.py
+++ setup.py
@@ -110,6 +110,7 @@ if __name__ == "__main__":
         #    ]
         # },
         include_package_data=True,
+        python_requires=">=3.8",
         classifiers=[
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.8",
