20b641a81b6531c11a9ca693bea227d5721853c9
michaelchin <michaelchin78@hotmail.com>
2025-03-10 18:58:15 +1100

    update 'packaging' before build pipy package

--- scripts/pypi-publish.sh
+++ scripts/pypi-publish.sh
@@ -5,7 +5,7 @@ echo "$BASEDIR"
 
 rm -rf $BASEDIR/../dist
 
-pip3 install build twine
+pip3 install -U build twine packaging
 python3 -m build
 twine check dist/*
 twine upload --non-interactive -u __token__ -p $1 dist/*
