9efd1e873ab178805cb22e13860a17c71630f60f
Michael Chin <michael.chin@sydney.edu.au>
2026-01-20 12:10:49 +1100

    initial change to 10-SeafloorGrids.ipynb and add a new file gmt_cpt.py

---
+++ gplately/mapping/gmt_cpt.py
@@ -0,0 +1,98 @@
+#
+#    Copyright (C) 2024-2026 The University of Sydney, Australia
+#
+#    This program is free software; you can redistribute it and/or modify it under
+#    the terms of the GNU General Public License, version 2, as published by
+#    the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful, but WITHOUT
+#    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#    for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+from matplotlib.colors import LinearSegmentedColormap, hsv_to_rgb
+
+
+def parse_old_cpt(row, values, colors, color_model="RGB"):
+    """parse one row of text from a cpt file in old format, such as below
+
+    0	210	0	0	10	210	0	0
+    10	230	40	0	20	230	40	0
+    20	245	60	0	30	245	60	0
+    30	255	98	0	40	255	98	0
+
+    """
+    values.append(float(row[0]))
+    values.append(float(row[4]))
+    if color_model == "RGB":
+        colors.append(
+            [
+                float(row[1]) / 255.0,
+                float(row[2]) / 255.0,
+                float(row[3]) / 255.0,
+            ]
+        )
+        colors.append(
+            [
+                float(row[5]) / 255.0,
+                float(row[6]) / 255.0,
+                float(row[7]) / 255.0,
+            ]
+        )
+    else:
+        colors.append(
+            list(hsv_to_rgb([float(row[1]) / 360.0, float(row[2]), float(row[3])]))
+        )
+        colors.append(
+            list(hsv_to_rgb([float(row[5]) / 360.0, float(row[6]), float(row[7])]))
+        )
+
+
+def parse_new_cpt(row, values, colors, color_model="RGB"):
+    """parse one row of text from a cpt file in old format, such as below
+
+    0               64/0/64         0.052632        64/0/192        L
+    0.052632        64/0/192        0.105263        0/64/255        L
+    0.105263        0/64/255        0.157895        0/128/255       L
+    0.157895        0/128/255       0.210526        0/160/255       L
+    0.210526        0/160/255       0.263158        64/192/255      L
+    """
+    # convert the new format into old format
+    color_1 = row[1].split("/")
+    color_2 = row[3].split("/")
+    parse_old_cpt(
+        [row[0]] + color_1 + [row[2]] + color_2, values, colors, color_model=color_model
+    )
+
+
+def get_cm_from_gmt_cpt(cpt_file):
+    """given a gmt cpt file path, return a matplotlib matplotlib.colors.LinearSegmentedColormap object"""
+    values = []
+    colors = []
+    color_model = "RGB"
+    with open(cpt_file, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            if "COLOR_MODEL" in line and "HSV" in line:
+                color_model = "HSV"
+            if line[0] in ["#", "B", "F", "N"]:
+                continue
+            vals = line.split()
+            if len(vals) == 8:
+                parse_old_cpt(vals, values, colors, color_model=color_model)
+            elif len(vals) == 5 or len(vals) == 4:
+                parse_new_cpt(vals, values, colors, color_model=color_model)
+
+    colour_list = []
+    # print(colors)
+    for i in range(len(values)):
+        colour_list.append(
+            ((values[i] - values[0]) / (values[-1] - values[0]), [x for x in colors[i]])
+        )
+    return LinearSegmentedColormap.from_list("cpt-cmap", colour_list)
