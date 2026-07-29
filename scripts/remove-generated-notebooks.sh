96cd0525342eaad5e93a64358915db0aee3ec26c
Michael Chin <michael.chin@sydney.edu.au>
2026-07-29 15:24:35 +1000

    add a helper script to remove all generated notebooks

---
+++ scripts/remove-generated-notebooks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm Notebooks/14-RuleBasedGPMLProcessingPipeline.ipynb
+rm Notebooks/Examples/hello_world.ipynb
+rm Notebooks/Examples/icosahedron_mesh.ipynb
+rm Notebooks/Examples/introducing_plate_model_manager.ipynb
+rm Notebooks/Examples/plot_map_with_cartopy.ipynb
+rm Notebooks/Examples/plot_map_with_pygmt.ipynb
+rm Notebooks/Examples/reconstruct_files.ipynb
+rm Notebooks/Examples/save_reconstructed_data.ipynb
+rm Notebooks/Examples/use_auxiliary_functions.ipynb
+rm Notebooks/Examples/use_your_own_plate_model.ipynb
+rm Notebooks/Examples/working_with_plate_model_manager.ipynb
