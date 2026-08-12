39962ae68305311b9a5c4f3c94b409c1c9fdf3a0
Michael Chin <michael.chin@sydney.edu.au>
2026-08-12 16:48:58 +1000

    remove some files which are not needed anymore

--- scripts/run_all_notebooks.sh
+++ scripts/run_all_notebooks.sh
@@ -16,15 +16,15 @@ notebooks=(
   13-ReconstructingZirconData
   14-RuleBasedGPMLProcessingPipeline
   15-ConvertGridReferenceFrame
-  Examples/plot_map_with_pygmt
-  Examples/hello_world
-  Examples/reconstruct_files
-  Examples/icosahedron_mesh
-  Examples/save_reconstructed_data
-  Examples/introducing_plate_model_manager
-  Examples/use_auxiliary_functions
-  Examples/plot_map_with_cartopy
-  Examples/use_your_own_plate_model
+  Examples/01-HelloWorld
+  Examples/02-PlateModelManager
+  Examples/03-PlotWithCartopy
+  Examples/04-PlotWithPyGMT
+  Examples/05-ReconstructFiles
+  Examples/06-LoadPlateModelFromFiles
+  Examples/07-SaveReconstructedData
+  Examples/08-UseAuxiliaryFunctions
+  Examples/09-IcosahedronMesh
 )
 
 ./scripts/run_notebooks.sh "${notebooks[@]}"
